ea19ec1aea8e98af45e9d55a2fd91e8870834c02
unknown <eli91ai@gmail.com>
2020-04-18 18:53:47 +0200

    redirect to in th app-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+const routes: Routes = [
 
-const routes: Routes = [];
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  //{ path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
